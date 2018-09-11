3af23de0e52d1599896c932c9730866c3dcf2448
Li Yang <yli587@best-inc.com>
2018-09-11 21:35:36 +0800

    activeStcokSpider

--- src/test/java/com/zoicapital/stockchartsfx/JSoupTest.java
+++ src/test/java/com/zoicapital/stockchartsfx/JSoupTest.java
@@ -15,16 +15,21 @@ public class JSoupTest {
 
     public static void main(String[] args) throws IOException {
         List<Article> list = new ArrayList<>();
-        String date = "09-10";
-        getArticles("000970",date,list);
+        String start = "09-11";
+        String end = "09-11";
+        getArticles("601330",start,end,1,list);
+        list.forEach(e ->{
+            System.out.println(e);
+        });
     }
 
-    private static void getArticles(String code, String date, List<Article> list) throws IOException {
-         String url ="http://guba.eastmoney.com/list,%s.html";
-        Document doc =   Jsoup.connect(String.format(url,code)).get();
+    private static void getArticles(String code, String start,String end, Integer pageNum ,List<Article> list) throws IOException {
+        String url ="http://guba.eastmoney.com/list,%s,f_%s.html";
+        Document doc =   Jsoup.connect(String.format(url,code,pageNum)).get();
         Element articles = doc.getElementById("articlelistnew");
         Elements allElements = articles.getElementsByClass("articleh");
-        for(int i = 0;i<allElements.size();i++){
+        int i = 0;
+        for( ;i<allElements.size();i++){
             Element elements= allElements.get(i);
             Elements title = elements.getElementsByClass("l3");
             Elements readCount = elements.getElementsByClass("l1");
@@ -39,10 +44,22 @@ public class JSoupTest {
             }
             Element titleEle = title.select("a[title]").get(0);
 
+            String date = create.get(0).text();
+            if(start.compareTo(date)>0 ||end.compareTo(date)<0 ){
+                break;
+            }
             Article article = new Article();
-            article.setDate(create.get(0).text());
+            article.setDate(date);
             article.setTitle(titleEle.attributes().get("title"));
             article.setReadCount(Integer.parseInt(readCount.get(0).text()));
+            list.add(article);
+        }
+
+        if(i==allElements.size()){
+            Article article = list.get(list.size() - 1);
+            if(article.getDate().compareTo(start)>= 0){
+                getArticles(code,start,end,++pageNum,list);
+            }
         }
     }
 }
